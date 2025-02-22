8727f1b7da19109fd798b4a3270c4608e1adaceb
Max Saunders <102803082+Maxsawdust@users.noreply.github.com>
2025-02-22 11:39:42 +0000

    create task-1 functionality

--- Nationalize and Weather API project/src/components/NationalizeTask/NationalizeTask.jsx
+++ Nationalize and Weather API project/src/components/NationalizeTask/NationalizeTask.jsx
@@ -4,7 +4,7 @@ export default function NationalizeTask() {
   // using ref to get the input element
   const inputRef = useRef(null);
   // using state to store the nationality and probability
-  const [nationality, setNationality] = useState(null);
+  const [nationality, setNationality] = useState("");
   // using effect to focus the input on render
   useEffect(() => {
     inputRef.current.focus();
@@ -15,21 +15,38 @@ export default function NationalizeTask() {
     e.preventDefault();
     // getting the user's name based on ref's value
     const name = inputRef.current.value;
-    // fetching data based on user's input
-    const response = await fetch(`https://api.nationalize.io?name=${name}`);
-    const data = await response.json();
+    // fetching nationalizeData based on user's input
+    const nationalizeResponse = await fetch(
+      `https://api.nationalize.io?name=${name}`
+    );
+    const nationalizeData = await nationalizeResponse.json();
+
+    // converting the probability to a percentage at 2DP
+    const probabilityPercentage = Number(
+      (nationalizeData.country[0].probability * 100).toFixed(2)
+    );
+
+    // convering the ISO code to country name using another API
+    const countryCode = nationalizeData.country[0].country_id;
+    const countriesResponse = await fetch(
+      `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/countries-codes/records?where=iso2_code%20%3D%20%22${countryCode}%22&limit=20`
+    );
+    const countriesData = await countriesResponse.json();
+    const countryLabel = countriesData.results[0].label_en;
 
     // using the setter to store the user's nationality in an object
     setNationality({
-      country: data.county[0].country_id,
-      probability: data.county[0].probability,
+      country: countryLabel,
+      probability: probabilityPercentage,
     });
   };
 
   return (
     <>
-      <h1 id="nationality">Your nationality is:</h1>
-      <h2 id="probability">With a probability of: </h2>
+      <h1 id="nationality">Your nationality is: {nationality.country}</h1>
+      <h2 id="probability">
+        With a probability of: {nationality.probability}%
+      </h2>
 
       <form id="input-form" onSubmit={guessNationality}>
         <p>Enter your name to estimate your nationality!</p>
